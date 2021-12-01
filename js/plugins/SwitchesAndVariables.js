93eee5240a9ae47d1f49e63e486c1b9a8c17da7c
VarVarKa <varyusha255@yandex.ru>
2021-12-01 05:10:41 +0300

    Добавлены основной сюжетный квест и автосохранения!

--- js/plugins/SwitchesAndVariables.js
+++ js/plugins/SwitchesAndVariables.js
@@ -45,3 +45,7 @@ function mix(arr) {
   for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
   return arr;
 }
+
+function cE(ce){
+	$gameTemp.reserveCommonEvent(ce)
+}
