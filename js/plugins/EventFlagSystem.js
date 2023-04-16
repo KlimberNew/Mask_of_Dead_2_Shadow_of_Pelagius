db07f66c68407ea5b59c7614a1ba2facfbf8616c
Klimber <egorkobrin1@gmail.com>
2023-04-17 01:17:56 +0300

    2.8.6. Внес фиксы багов от Вайти
    - Починен баг со сбросом флагов, при переходе на другую локацию.
    - Починен баг с отсутствием возможности присвоить флаг событию в нем же через параллельный триггер

--- js/plugins/EventFlagSystem.js
+++ js/plugins/EventFlagSystem.js
@@ -7,24 +7,46 @@
  * @author WhitePaper
  *
  * @help 
- * isFlag('dead') - перевірка прапору 'dead' цієї події
+ * isFlag('dead', this) - перевірка прапору 'dead' цієї події
  * isFlag('dead', 1) - перевірка прапору 'dead' події 1
  * 
- * setFlag('dead') - запис значення true прапору 'dead' цієї події
+ * setFlag('dead', this) - запис значення true прапору 'dead' цієї події
  * setFlag('dead', 1) - запис значення true прапору 'dead' події 1
  * setFlag('dead', 1, false) - запис значення false прапору 'dead' події 1
+ * 
+ * $gameSystem._eventFlags[2][1]['dead'] - прапор 'dead' події 1 карти 2
  */
 
-function isFlag(flag, id=$gameMap._interpreter._eventId){
-    return $gameMap.event(id)._eventFlags[flag] || false;
+
+
+function isFlag(flag, id){
+    if (!Number(id)){
+        id = id._eventId;
+    }
+    if (!$gameSystem._eventFlags[$gameMap._mapId]){
+        return false;
+    }
+    if (!$gameSystem._eventFlags[$gameMap._mapId][id]){
+        return false;
+    }
+    return $gameSystem._eventFlags[$gameMap._mapId][id][flag] || false;
 }
 
-function setFlag(flag, id=$gameMap._interpreter._eventId, value=true){
-    $gameMap.event(id)._eventFlags[flag] = value;
+function setFlag(flag, id, value=true){
+    if (!Number(id)){
+        id = id._eventId;
+    }
+    if (!$gameSystem._eventFlags[$gameMap._mapId]){
+        $gameSystem._eventFlags[$gameMap._mapId] = {};
+    }
+    if (!$gameSystem._eventFlags[$gameMap._mapId][id]){
+        $gameSystem._eventFlags[$gameMap._mapId][id] = {};
+    }
+    $gameSystem._eventFlags[$gameMap._mapId][id][flag] = value;
 }
 
-EventFlagSystem_Game_Event_initMembers = Game_Event.prototype.initMembers;
-Game_Event.prototype.initMembers = function() {
-    EventFlagSystem_Game_Event_initMembers.call(this);
+EventFlagSystem_Game_System_prototype_initialize = Game_System.prototype.initialize;
+Game_System.prototype.initialize = function() {
+    EventFlagSystem_Game_System_prototype_initialize.apply(this);
     this._eventFlags = {};
-};
+}
