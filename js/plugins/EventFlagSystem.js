be8e6fc5618f8388db6ad4df122c816628212500
WhitePaperChan <Firerosealicorn@gmail.com>
2024-04-19 17:45:02 +0300

    Стрелок (ТЕСТ) фикс, EventFlagSystem обнова, - фон битвы главы 2
    - Стрелок (ТЕСТ) тепер мертв на карте после смерти в бою,
    - (EventFlagSystem) функции isFlag и setFlag принимают номер карты,
    - убрано фоновые битвы с карт {18 Северная часть каньона}, {18+ Северная часть каньона}, {19 Северная часть каньона}

--- js/plugins/EventFlagSystem.js
+++ js/plugins/EventFlagSystem.js
@@ -9,40 +9,42 @@
  * @help 
  * isFlag('dead', this) - перевірка прапору 'dead' цієї події
  * isFlag('dead', 1) - перевірка прапору 'dead' події 1
+ * isFlag('dead', 1, 2) - перевірка прапору 'dead' події 1 карти 2
  * 
  * setFlag('dead', this) - запис значення true прапору 'dead' цієї події
  * setFlag('dead', 1) - запис значення true прапору 'dead' події 1
  * setFlag('dead', 1, false) - запис значення false прапору 'dead' події 1
+ * setFlag('dead', 1, false, 2) - запис значення false прапору 'dead' події 1 карти 2
  * 
  * $gameSystem._eventFlags[2][1]['dead'] - прапор 'dead' події 1 карти 2
  */
 
 
 
-function isFlag(flag, id){
+function isFlag(flag, id, map=$gameMap._mapId){
     if (!Number(id)){
         id = id._eventId;
     }
-    if (!$gameSystem._eventFlags[$gameMap._mapId]){
+    if (!$gameSystem._eventFlags[map]){
         return false;
     }
-    if (!$gameSystem._eventFlags[$gameMap._mapId][id]){
+    if (!$gameSystem._eventFlags[map][id]){
         return false;
     }
-    return $gameSystem._eventFlags[$gameMap._mapId][id][flag] || false;
+    return $gameSystem._eventFlags[map][id][flag] || false;
 }
 
-function setFlag(flag, id, value=true){
+function setFlag(flag, id, value=true, map=$gameMap._mapId){
     if (!Number(id)){
         id = id._eventId;
     }
-    if (!$gameSystem._eventFlags[$gameMap._mapId]){
-        $gameSystem._eventFlags[$gameMap._mapId] = {};
+    if (!$gameSystem._eventFlags[map]){
+        $gameSystem._eventFlags[map] = {};
     }
-    if (!$gameSystem._eventFlags[$gameMap._mapId][id]){
-        $gameSystem._eventFlags[$gameMap._mapId][id] = {};
+    if (!$gameSystem._eventFlags[map][id]){
+        $gameSystem._eventFlags[map][id] = {};
     }
-    $gameSystem._eventFlags[$gameMap._mapId][id][flag] = value;
+    $gameSystem._eventFlags[map][id][flag] = value;
 }
 
 EventFlagSystem_Game_System_prototype_initialize = Game_System.prototype.initialize;
